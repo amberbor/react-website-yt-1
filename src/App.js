47de6c42a0b4e0af7098e070074a2461e2a1c340
Amber <amber.borici@y42.com>
2023-09-27 16:57:53 +0200

    creating home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+
+import { BrowserRouter, Routes , Route} from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
